fba8de5bb14783a66fcf4c9528b00a62cd27d1cf
Rajeshkumar2628 <rajeshregins@gmail.com>
2024-05-03 12:58:39 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import About from './components/About';
+import './components/style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <About/>
+    </div>
+  );
+}
+
+export default App;
